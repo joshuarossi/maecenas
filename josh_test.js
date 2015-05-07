b14a4e0316c631fac2947af297c027a1e5266db9
joshuarossi <joshua.rossi@icloud.com>
2015-05-06 20:17:56 -0500

    first commit

---
+++ josh_test.js
@@ -0,0 +1,93 @@
+Schema = {};
+
+
+Schema.UserProfile = new SimpleSchema({
+  firstName: {
+    type: String,
+    regEx: /^[a-zA-Z-]{2,25}$/,
+    optional: true
+  },
+  lastName: {
+    type: String,
+    regEx: /^[a-zA-Z]{2,25}$/,
+    optional: true
+  },
+  website: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Url,
+    optional: true
+  },
+  bitcoin_address: {
+    type: String,
+    optional: true
+  }
+});
+
+Schema.User = new SimpleSchema({
+  username: {
+    type: String,
+    regEx: /^[a-z0-9A-Z_]{3,15}$/
+  },
+  emails: {
+    type: [Object],
+    // this must be optional if you also use other login services like facebook,
+    // but if you use only accounts-password, then it can be required
+    optional: true
+  },
+  "emails.$.address": {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email
+  },
+  "emails.$.verified": {
+    type: Boolean
+  },
+  createdAt: {
+    type: Date
+  },
+  profile: {
+    type: Schema.UserProfile,
+    optional: true
+  },
+  services: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  },
+  // Option 2: [String] type
+  // If you are sure you will never need to use role groups, then
+  // you can specify [String] as the type
+  roles: {
+    type: [String],
+    optional: true
+  }
+});
+
+Meteor.users.attachSchema(Schema.User);
+
+Router.route('/', function () {
+  this.render('Home');
+});
+
+Router.route('/about');
+
+Router.route('/plugin/:username', function () {
+  this.render('plugin', {
+    data: function () {
+      console.log('i am about to return user');
+      return this.params.username;
+    }
+  });
+});
+
+if (Meteor.isClient) {
+  console.log('Starting up from the console...');
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    console.log('Started up the server...')
+  });
+}
