cfd438e7c7a4141cb8ad9e3c901a3c055c7cdb8d
mrdunne <wdunne1@pca.ac.uk>
2015-05-07 16:54:07 +0100

    custom user interface, better structure

--- maecenas.js
+++ maecenas.js
@@ -1,88 +1,5 @@
-Schema = {};
-
-Schema.UserProfile = new SimpleSchema({
-    firstName: {
-        type: String,
-        regEx: /^[a-zA-Z-]{2,25}$/,
-        optional: true
-    },
-    lastName: {
-        type: String,
-        regEx: /^[a-zA-Z]{2,25}$/,
-        optional: true
-    },
-    website: {
-        type: String,
-        regEx: SimpleSchema.RegEx.Url,
-        optional: true
-    },
-    bitcoin_address: {
-        type: String,
-        optional: true
-    }
-});
-
-Schema.User = new SimpleSchema({
-    username: {
-        type: String,
-        regEx: /^[a-z0-9A-Z_]{3,15}$/
-    },
-    emails: {
-        type: [Object],
-        // this must be optional if you also use other login services like facebook,
-        // but if you use only accounts-password, then it can be required
-        optional: true
-    },
-    "emails.$.address": {
-        type: String,
-        regEx: SimpleSchema.RegEx.Email
-    },
-    "emails.$.verified": {
-        type: Boolean
-    },
-    createdAt: {
-        type: Date
-    },
-    profile: {
-        type: Schema.UserProfile,
-        optional: true
-    },
-    services: {
-        type: Object,
-        optional: true,
-        blackbox: true
-    },
-    roles: {
-        type: [String],
-        optional: true
-    }
-});
-
-Meteor.users.attachSchema(Schema.User);
-
-Router.configure({
-    layoutTemplate: 'layout'
-});
-Router.route('/', function () {
-    this.render('home');
-});
-
-Router.route('/about');
-
-Router.route('/plugin/:username', function () {
-    this.render('plugin', {
-        data: function () {
-            return this.params.username;
-        }
-    });
-});
-
 if (Meteor.isClient) {
     console.log('Starting up from the console...');
-    Accounts.ui.config({
-        passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
-    });
-
 }
 
 if (Meteor.isServer) {
@@ -90,3 +7,4 @@ if (Meteor.isServer) {
         console.log('Started up the server...')
     });
 }
+
