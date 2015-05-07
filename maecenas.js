dfa31dd2ca7acbc1069ef0a0288583010efaa015
mrdunne <wdunne1@pca.ac.uk>
2015-05-07 17:49:32 +0100

    More

--- maecenas.js
+++ maecenas.js
@@ -7,4 +7,17 @@ if (Meteor.isServer) {
         console.log('Started up the server...')
     });
 }
-
+Meteor.startup(function() {
+    // Add Facebook configuration entry
+    ServiceConfiguration.configurations.update(
+      { "service": "facebook" },
+      {
+        $set: {
+          "appId": "1639777159574816",
+          "secret": "8de6f0f33b4c5276ccd82637bdbbb135"
+        }
+      },
+      { upsert: true }
+    );
+   
+});
