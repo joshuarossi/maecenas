e986179eb3ba3dc861fed35a38f4e10fba308a18
mrdunne <wdunne1@pca.ac.uk>
2015-05-09 01:10:11 +0100

    origins of gem

---
+++ server/server.js
@@ -0,0 +1,16 @@
+if (Meteor.isServer) {
+  Meteor.methods({
+    'getAddress': function getAddress() {
+      var roundnode = Meteor.npmRequire('round-node');
+
+      Round.client(options, function(error, client) {
+        
+      });
+
+      
+      });
+
+      return gists.result;
+    }
+  });
+}
