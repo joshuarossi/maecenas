ebeb5f59473cfa895638d250a62c361f5da96984
mrdunne <wdunne1@pca.ac.uk>
2015-05-12 01:42:27 +0100

    coinkite start

--- server/server.js
+++ server/server.js
@@ -1,16 +1,9 @@
 if (Meteor.isServer) {
   Meteor.methods({
     'getAddress': function getAddress() {
-      var roundnode = Meteor.npmRequire('round-node');
-
-      Round.client(options, function(error, client) {
-        
-      });
-
-      
-      });
-
-      //return something
+      var kite = Meteor.npmRequire('coinkite-javascript'); 
+      var sign = kite.auth_headers('Kd3f45974-9b2c5376-3eefd77f2f28470d', 'Sacf772f4-d07889c6-197197aa531a43b7', '/v1/new/receive');
+      Meteor.http.call("PUT", "http://api.coinkite.com/v1/new/receive", { params: { 'X-CK-Key': sign['X-CK-Key'], 'X-CK-Sign': sign['X-CK-Sign'], 'X-CK-Timestamp': sign['X-CK-Timestamp'] }});
     }
   });
 }
