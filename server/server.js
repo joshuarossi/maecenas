0aeab1e59a379f39a40b5d1d69596fc407aef205
mrdunne <wdunne1@pca.ac.uk>
2015-05-15 18:40:55 +0100

    Round

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ Transactions = new Mongo.Collection("transactions");
 
 function didWin(amount)
 {
-	var chances = amount * 100000000;
+	var chances = Math.round(amount * 1000);
 	console.log(chances);
 	var pot = Pot.findOne().balance;
 	console.log(pot);
