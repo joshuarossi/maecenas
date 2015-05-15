6d7acfa59d73aa534d94b685ca2ee3b81f7b1112
mrdunne <wdunne1@pca.ac.uk>
2015-05-15 21:58:35 +0100

    1/5

--- server/server.js
+++ server/server.js
@@ -4,15 +4,26 @@ Transactions = new Mongo.Collection("transactions");
 
 function didWin(amount)
 {
-	var chances = Math.round(amount * 1000);
+	var chances = Math.round(amount * 10000);
 	console.log(chances);
 	var pot = Pot.findOne().balance;
+	var potchance = Math.round(pot * 1000);
 	console.log(pot);
+	var rand = Math.round(Math.random() * 5);
+	
+	if(rand = 5)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 };
 
 Meteor.startup(function(){
 	Pot.insert({
 		balance: 32
 	});
-	didWin(0.10003);
+	console.log(didWin(0.10003));
 });
