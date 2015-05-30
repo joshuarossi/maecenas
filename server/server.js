0b6bc6c5f29c35df749ba4e2e6657d8088861ed9
mrdunne <wdunne1@pca.ac.uk>
2015-05-30 01:48:55 +0100

    further node test..

--- server/server.js
+++ server/server.js
@@ -1,7 +1,40 @@
+
+
+if (Meteor.isClient) {
+  testNode = function testNode(callback) {
+    Meteor.call('testNode', callback);
+  }
+}
+
 if (Meteor.isServer) {
   Meteor.methods({
-    'testNode': function testNode(user) {
-      var node = Meteor.npmRequire('round-node');
+    'testNode': function testNode() {
+      var Round = Meteor.npmRequire('round-node');
+      Round.client(function (error, client) {
+      var creds = {
+        api_token: "znpyzF7QoLWweBid2oy-iIoBsocBVRIZIlzl4KHGm8Y",
+        admin_token: "bQkz35qDfbs0I3ks41QprdLZ2tl5q_rlvHK4mZB3x6Q",
+        totp_secret: "yjztn5drz4ewfmu7"
+      }
+      client.authenticate_application(creds, function (error, application) {
+        application.wallets(function(error, wallets) {
+          walletInfo = {
+            name: 'wallet' + Math.random(),
+            passphrase: 'password' 
+          }
+          wallets.create(walletInfo, function(error, wallet) {
+            wallet.accounts(function(error, accounts) {
+              var account = accounts.get(0)
+              account.addresses(function(error, addresses) {
+                addresses.create(function(error, address) {
+                  console.log(address.string)
+                });
+              });
+            });
+          });
+        });
+      });
+    });
       
     }
   });
